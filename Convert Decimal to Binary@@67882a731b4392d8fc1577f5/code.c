8f0a81be9f8ae0174f6f182bfc0c258c9838d6b3
zefebyte <9141.uspc@gmail.com>
2025-02-05 13:59:12 +0530

    Solution to Convert Decimal to Binary

--- Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -9,8 +9,9 @@ int main() {
     int i = 0;    // Exponent for pow(10, i)
 
     while (n != 0) {
-        int bit = n & 1;           // Extract the least significant bit
-        ans = ans + (bit * int pow(10, i));  // Use pow(10, i) to calculate place value
+        int bit = n & 1;  
+        int z = pow(10,i)   ;      // Extract the least significant bit
+        ans = ans + (bit *z );  // Use pow(10, i) to calculate place value
         n = n >> 1;  // Right shift n to process the next bit
         i++;  // Increment i to move to the next place value (1, 10, 100, etc.)
     }
