d9612dd54623c345dbe4552f6f9565f714f9acae
zefebyte <9141.uspc@gmail.com>
2025-02-06 16:53:53 +0530

    Solution to Check Leap Year

--- Check Leap Year@@6788dd63490637cfc44e1b90/code.c
+++ Check Leap Year@@6788dd63490637cfc44e1b90/code.c
@@ -5,10 +5,10 @@
 int main() {
     int n;
     scanf("%d",&n);
-    if(n%4==0){
+    if(n%4==0 && n%100!=0){
         printf("Leap Year");
     }
-    else if(n%400==0 && n%100!=0){
+    else if(n%400==0){
         printf("Leap Year");
     }
     else{
