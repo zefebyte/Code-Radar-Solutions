f6374a04b687a4a2191f247da9f29b2397c3194b
Abhay dhasmana <9141.uspc@gmail.com>
2025-02-18 00:45:17 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%c",&a){
+        if(a=='R'){
+            printf("Stop");
+        }
+        else if(a=='G'){
+            printf("Slow Down");
+        }
+        else if(a=='Y'){
+            printf("Slow Down");
+        }
+        else{
+            printf("Invalid");
+        }
+    }
+    
+
+   
+}
