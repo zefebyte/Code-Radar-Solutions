8ac32e7ceaf839ea83d51856e8b9ec7c42e025b4
Abhay dhasmana <9141.uspc@gmail.com>
2025-02-18 00:26:33 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,&c);
+    if(c=='+'){
+        printf("%d",a+b);
+    }
+    else if(c=='-'){
+        printf("%d",a-b);
+    }
+    else if(c=='*'){
+        printf("%d",a*b);
+    }
+    else{
+        printf("%d",a/b);
+    }
+
+}
