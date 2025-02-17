601bc0d5321756103a1416acc35170ec180e24cb
Abhay dhasmana <9141.uspc@gmail.com>
2025-02-18 00:42:16 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,35 @@
+// Your code here...
+// Your code here...
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char c;
+    scanf("%c",&c);
+    if(c=='A'){
+        printf("Excellent");
+    }
+      else if(c=='B'){
+        printf("Good");
+    }
+      else if(c=='C'){
+        printf("Average");
+    }
+      else if(c=='D'){
+        printf("Below Average");
+    }
+      else if(c=='F'){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid grade");
+    }
+ 
+
+
+
+
+
+    }
+
+    
