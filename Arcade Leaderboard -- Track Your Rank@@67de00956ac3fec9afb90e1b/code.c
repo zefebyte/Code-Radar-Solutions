3703d9adc055690fd850ade85fe5d4821fb32cb6
Abhay dhasmana <9141.uspc@gmail.com>
2025-03-28 17:37:00 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,22 +1,15 @@
-// Your code here...
-// Online C compiler to run C program online
 #include <stdio.h>
-void trackPlayerRanks(int ranked[],int n,int player[],int m,int result[]){
-    int a=0;
-    for(int i=0;i<m;i++){
-        int rankfound =0;
-        for(int j=0;j<n;j++){
-            if(player[i]>=ranked[j]){
-                result[a]=j+1;
-                a++;
-                rankfound =1;
-                break;
-                
+
+void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
+    for (int i = 0; i < m; i++) { // Iterate through each player
+        int rank = 1; // Start rank at 1
+        for (int j = 0; j < n; j++) { // Iterate through the leaderboard
+            if (player[i] < ranked[j]) { // If player score is less than current ranked score
+                rank++;
+            } else { // If player score is greater or equal
+                break; // No need to continue, rank is found
             }
         }
-        if(!rankfound){
-            result[a] =n+1;
-            a++;
-        }
+        result[i] = rank; // Store the player's rank
     }
 }
